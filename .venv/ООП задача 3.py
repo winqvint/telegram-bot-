e1af763a8331abb92c7ad233ead62caa6793a988
Виктория Рыбалко <viktoriarybalko@MacBook-Air-Viktoria.local>
2025-10-04 22:51:52 +0300

    Remove .venv directory from repository

--- .venv/ООП задача 3.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def set_private_key(self,private_key):
-        self.private_key = private_key
-    def show_private_key(self):
-        print(f'Приватный ключ пользователя:{self.private_key}')
-user1 = User()
-user1.set_private_key('uox00b_12x')
-user1.show_private_key()
